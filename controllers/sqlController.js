d6a4929f7a27377d42b1b6c3eb40f36df69e31f5
SirawitN <nine_sirawit@hotmail.com>
2021-11-20 00:46:21 +0700

    sql_sqperate_module_yourRideAdded

---
+++ controllers/sqlController.js
@@ -0,0 +1,18 @@
+const connection = require('../db/mySQL');
+
+const yourRides =
+  'SELECT route, starting_time, max_available_seats, reserved_passengers FROM ride WHERE driver_id = ?';
+
+const yourRide = (req, res) => {
+  const id = req.body.id;
+  connection.query(yourRides, id, (err, result) => {
+    if (err) {
+      res.status(500);
+      return;
+    }
+
+    res.json(result);
+  });
+};
+
+module.exports = { yourRide };
---
+++ db/mySQL.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const fs = require('fs');
+require('dotenv').config();
+
+const mysqlHost = process.env.MYSQL_HOST;
+const mysqlPort = process.env.MYSQL_PORT;
+const mysqlUser = process.env.MYSQL_USER;
+const mysqlPW = process.env.MYSQL_PASSWORD;
+const mysqlDB = process.env.MYSQL_DB;
+
+const connection = mysql.createPool({
+  host: mysqlHost,
+  user: mysqlUser,
+  password: mysqlPW,
+  database: mysqlDB,
+  port: mysqlPort,
+  ssl: {
+    ca: fs.readFileSync('../BaltimoreCyberTrustRoot.crt.pem'),
+  },
+});
+
+module.exports = connection;
