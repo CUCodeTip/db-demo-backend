6015363ab68580fb703fa07850c05768fd01d296
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-11-20 15:11:09 +0700

    remove sqlController

--- routes/sqlRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const sqlController = require('../controllers/sqlController');
-const router = express.Router();
-
-router.post('/rides', sqlController.yourRide);
-
-module.exports = router;
